f1f0ebe754950b1efd53d7b60ae6dc491c58d6c6
Aceberg <>
2026-02-28 23:28:52 +0800

    some update web login endpoint func

--- web/src/utils/api.js
+++ web/src/utils/api.js
@@ -65,7 +65,7 @@ export const login = async (username, password) => {
     url = ':8000/api/login';
   } else {
     // 生产环境使用完整URL（包含端口8000）
-    url = `${window.location.protocol}//${window.location.hostname}:8000/api/login`;
+    url = `${window.location.protocol}//${window.location.hostname}/api/login`;
   }
   
   try {
